ac1fa62b0263d550146fd5f087e6734afedf5dd3
박단비 <qkreksqhd@naver.com>
2025-03-06 21:57:37 +0900

    solved 디스크컨트롤러 - 0.57ms 10.4mb

---
+++ Programmers/디스크컨트롤러/디스크컨트롤러_박단비.py
@@ -0,0 +1,27 @@
+# 현재 시점에서 처리할 수 있는 작업들을 힙에 넣기
+from heapq import heappush, heappop
+
+
+def solution(jobs):
+    answer = 0
+    jobs.sort()
+
+    N = len(jobs)
+    heap = []
+    idx = 0
+    now = 0  # 현재시간
+
+    while idx < N or heap:
+        while idx < N and now >= jobs[idx][0]:
+            heappush(heap, (jobs[idx][1], jobs[idx][0], idx))
+            idx += 1
+
+        if heap:
+            time, request, index = heappop(heap)
+            now += time
+            answer += (now - request)
+
+        else:
+            now = jobs[idx][0]
+
+    return answer // N
