c68d3a46392e3ab3c74db29633933cf5bfd9f70a
Felix0942364 <122508523+Felix0942364@users.noreply.github.com>
2025-03-19 20:59:22 +0900

    Create 타일채우기_조재웅.java
    solved 타일채우기 - 108ms 14316kb

---
+++ Baekjoon/타일채우기/타일채우기_조재웅.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        if (N % 2 != 0)
+        {
+            System.out.println(0); // if N is odd it can't be filled with size 2 blocks
+        }
+        else
+        {
+            N = N / 2;
+            int[] arr = new int[N];
+            arr[0] = 3;
+            for(int i = 1; i < N; i++)
+            {
+                int value = 0;
+                for (int j = 0; j < i - 1; j++)
+                {
+                    value += 2 * arr[j];
+                }
+                value += 3 * arr[i-1];
+                value += 2;
+                arr[i] = value;
+            }
+            System.out.println(arr[N-1]);
+        }
+    }
+}
