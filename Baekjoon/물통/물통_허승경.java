14d05bbbf0ae858729ba6911399ff8ffaa415382
skyungg <167261107+skyungg@users.noreply.github.com>
2025-03-21 14:50:47 +0000

    solved 물통 116ms 14.644mb

--- Baekjoon/물통/물통_허승경.java
+++ Baekjoon/물통/물통_허승경.java
@@ -1,75 +1,67 @@
 /*
- * 아이디어 : 구현
- * 1. a에 먼저 부을 때
- * - a에 옮긴양을 b에 전부 옮길 수 있음 -> 성공 -> 현재 c 용량 추가
- * - 현재 c용량을 a에 옮길 수 있음 && c 용량을 b에 옮길 수 있음 && b 용량을 c에 옮길 수 있음 -> 현재 b 용량 추가
- * 2. b에 먼저 부을 때
- * - 현재 c에 남은 용량 추가
- * - 현재 c의 용량 a에 전부 옮길 수 있음 && b에 용량 전부 c에 옮길 수 있음 && 현재 c의 용량 b에 옮길 수 있음 -> 현재 b의 용량 추가
+ * 아이디어 : bfs
  * */
+
 import java.io.*;
 import java.util.*;
 
 public class Main {
-
+	static boolean [][] visited = new boolean[201][201];		// A물병에 a만큼, B물병에 b만큼 있는 상태
+	static boolean [] answer = new boolean[201]; 
+	static int [] arr;
 	public static void main(String[] args) throws IOException{
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int [] arr = new int[3];
+		arr = new int[3];
 		for(int i = 0; i < 3; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		Set<Integer> set = new HashSet<>();
-		set.add(arr[2]);
+		// 탐색
+		bfs();
 		
-		// 구현
-		for(int i = 0; i < 2; i++) {
-			int [] tmp = new int[3];
-			if(i == 0) {
-				if(arr[0] >= arr[2]) {
-					tmp[0] = arr[2];	// 전부 붓기
-				}else {
-					tmp[0] = arr[0];	// a 최대 용량 붓기
-				}
-				tmp[2] = arr[2]-tmp[0];
-				
-				if(arr[1] >= tmp[0]) {
-					tmp[1] = tmp[0];	// 전부 붓기
-					tmp[0] = 0;
-				}else {
-					tmp[1] = arr[1];	// a 최대 용량 붓기
-					tmp[2] += (tmp[0]-tmp[1]);
-					tmp[0] = 0;
-				}
-				set.add(tmp[2]);
-				
-				if(arr[1] >= tmp[2] && arr[2] >= tmp[1] && arr[0] >=tmp[2]) {
-					set.add(tmp[1]);
-				}
-			}else {
-				if(arr[1] >= arr[2]) {
-					tmp[1] = arr[2];	// 전부 붓기
-				}else {
-					tmp[1] = arr[1];	// a 최대 용량 붓기
-				}
-				tmp[2] = arr[2]-tmp[1];
-				set.add(tmp[2]);
-				if(arr[0] >= tmp[2] && arr[2] >= tmp[1] && arr[1] >=tmp[2]) {
-					set.add(tmp[1]);
-				}
-			}
-		}
-		
-		List<Integer> list = new ArrayList<>(set);
-		Collections.sort(list);
 		StringBuilder sb = new StringBuilder();
-		for(int num : list) {
-			sb.append(num+" ");
+		for(int i = 0; i < 201; i++) {
+			if(answer[i]) sb.append(i+ " ");
 		}
 		
 		System.out.println(sb.toString());
+	}
+	
+	static void bfs() {
+		Queue<int []> que = new LinkedList<>();
+		que.add(new int[] {0, 0, arr[2]});	// 초기화
+		visited[0][0] = true;
+		
+		while (!que.isEmpty()) {
+            int[] cur = que.poll();
+            int a = cur[0];		//a
+            int b = cur[1];		//b
+            int c = cur[2];		//c
+
+            if (a == 0) answer[c] = true;	// a비워진 상태 -> c값 확인
+
+            // 6가지 물 붓기 시도
+            for (int i = 0; i < 3; i++) {		// (i -> j)로 물 붓기
+                for (int j = 0; j < 3; j++) {
+                    if (i == j) continue;
+
+                    int[] next = {a, b, c};
+                    int amount = Math.min(next[i], arr[j] - next[j]);
+
+                    next[i] -= amount;		// 시작 물병 (물 감소)
+                    next[j] += amount;		// 도착 물병 (물 추가)
+
+                    if (!visited[next[0]][next[1]]) {
+                        visited[next[0]][next[1]] = true;		// a물병에 next[0]만큼, b물병에 next[1]만큼 있음
+                        que.add(new int[]{next[0], next[1], next[2]});
+                    }
+                }
+            }
+        }
+		
+		
 	}
 
 }
