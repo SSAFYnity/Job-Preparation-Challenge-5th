60d3da36cfe50013f898bcf225748854266b06a4
skyungg <167261107+skyungg@users.noreply.github.com>
2025-03-27 05:43:52 +0000

    solved 불량사용자 - 119.25ms 105mb

--- Programmers/불량사용자/불량사용자_허승경.java
+++ Programmers/불량사용자/불량사용자_허승경.java
@@ -2,61 +2,66 @@ import java.util.*;
 
 /*
 아이디어: 일단 구현
-1. user_id 배열을 <길이, 단어들> hashmap에 저장
-2. 제재 아이디 목록 돌면서 해당 아이디 길이의 value 찾기
-3. value에서 조건 맞는 단어 있으면 인덱스에 해당하는 배열 카운트 1, set 저장
-4. 이미 set에 있으면 넘어가~~
-5. 배열값 곱해버려~~
+1. b_id에 해당하는 u_id를 list에 저장하기 -> List<List<String>> -> 어차피 순서대로 하니까 map 안씀
+2. 위의 list 가지고 순열조합 만드고, set에 넣기
+3. set.size()가 정답
 */
 
 class Solution {
-    HashMap<Integer, ArrayList<String>> hmap = new HashMap<>();
+    List<List<String>> list = new ArrayList<>();
+    Set<Set<String>> set = new HashSet<>();
+    
     public int solution(String[] user_id, String[] banned_id) {
-        Set<String> set = new HashSet<>();
         
-        for(String id : user_id){
-            int len = id.length();
-            if(!hmap.containsKey(len)){
-                ArrayList<String> list = new ArrayList<>();
-                list.add(id);
-                hmap.put(len, list);
-            }else{
-                hmap.get(len).add(id);
-            }
+        for(int i = 0; i < banned_id.length; i++){
+            list.add(new ArrayList<>());
         }
         
-        int [] countArr = new int[banned_id.length];
-        for(int i = 0; i < banned_id.length; i++){
-            String target = banned_id[i];
+        // 1. b_id에 해당하는 u_id의 리스트 
+        for(int idx = 0; idx < banned_id.length; idx++){
+            String target = banned_id[idx];
             
-            if(hmap.containsKey(target.length())){
-                ArrayList<String> list = hmap.get(target.length());
-                for(int j = 0; j < list.size(); j++){
+            for(String str : user_id){
+                if(str.length() == target.length()){
                     boolean flag = true;
-                    for(int k = 0; k < target.length(); k++){
-                        if(target.charAt(k) == '*') continue;
-                        else{
-                            if(target.charAt(k) != list.get(j).charAt(k)){
-                                flag = false;
-                                break;
-                            }
-                        }
+                    
+                    for(int j = 0; j < target.length(); j++){
+                        if(target.charAt(j) == '*') continue;
+                        if(target.charAt(j) != str.charAt(j)){
+                            flag = false;
+                            break;
+                         }
                     }
+                    
                     if(flag){
-                        if(!set.contains(list.get(j))){
-                            set.add(list.get(j));
-                            countArr[i]++;
-                        }
+                        list.get(idx).add(str);
                     }
                 }
             }
         }
         
-        int answer = 1;
-        for(int num : countArr){
-            if(num == 0) continue;
-            else answer*=num;
+        // 2. 순열 조합 구하기
+        backtracking(0, new HashSet<>());
+        
+        // 3. 정답 출력
+        return set.size();
+        
+    }
+    
+    void backtracking(int depth, Set<String> curSet){
+        // 가지치기
+        if(depth == list.size()){        // 하나 조합 만들어진 경우
+            set.add(new HashSet<>(curSet));
+            return;   
         }
-        return answer;
+        
+        
+        for(String tmp : list.get(depth)){
+            if(curSet.contains(tmp)) continue;
+            
+            curSet.add(tmp);
+            backtracking(depth+1, curSet);
+            curSet.remove(tmp);
+        }
     }
 }
